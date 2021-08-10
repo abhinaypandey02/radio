36f7b2b81f3e8c2dfddb2c77b7b67d9edb121352
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-08-10 23:24:30 +0530

    chat

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -3,77 +3,142 @@ import ReactHowler from "react-howler";
 import {io, Socket} from 'socket.io-client';
 import CurrentlyPlaying from "../interfaces/currentlyPlaying";
 import {DefaultEventsMap} from "socket.io-client/build/typed-events";
+import {Button, Form, Modal} from "react-bootstrap";
+import {Chat} from "../interfaces/Chat";
+import {v4} from 'uuid';
 
-const SYNC_RATE=2000;
+const SYNC_RATE = 2000;
+
+function ChatBox({chats, sendMessage, name}: { chats: Chat[], sendMessage: (message: string) => void, name: string }) {
+    const [message, setMessage] = useState("");
+    return <div>
+        <div id="chatBox" className="overflow-auto my-3 p-2" style={{maxHeight: "60vh"}}>
+            {chats.map(chat => <div className={"my-3 " + (chat.sender === name ? "text-end" : "text-start")}
+                                    key={chat.id}>
+                <div style={{wordBreak: "break-all"}}>{chat.message}</div>
+                <small className="fw-light">{chat.sender} | {chat.time}</small>
+                <hr/>
+            </div>)}
+        </div>
+
+        <Form onSubmit={() => {
+            setMessage("");
+            sendMessage(message)
+        }} className="d-flex">
+            <Form.Control className="me-2" value={message} onChange={e => setMessage(e.target.value)}
+                          placeholder="Write a message"/>
+            <Button type="submit">Send</Button>
+        </Form>
+
+    </div>
+}
+
 
 export default function Player() {
     const [currSong, setCurrSong] = useState<CurrentlyPlaying>();
     const [isPlaying, setIsPlaying] = useState(false);
     const [loaded, setLoaded] = useState(false);
-    const [currentState,setCurrentState]=useState<"X"|"W"|"P">("X");
-    const [interacted,setInteracted]=useState(false);
+    const [currentState, setCurrentState] = useState<"X" | "W" | "P">("X");
+    const [interacted, setInteracted] = useState(false);
     const playerRef: any = useRef(null);
-    const [name,setName]=useState<string>();
-    const [socket,setSocket]=useState<Socket<DefaultEventsMap, DefaultEventsMap>>();
-
-    useEffect(()=>{
-        if(!socket) {
-            const socket=io("http://34.131.60.7:3001");
+    const [name, setName] = useState<string>("");
+    const [isNameModalOpen, setIsNameModalOpen] = useState(false);
+    const [isChatModalOpen, setIsChatModalOpen] = useState(false);
+    const [socket, setSocket] = useState<Socket<DefaultEventsMap, DefaultEventsMap>>();
+    const [chats, setChats] = useState<Chat[]>([]);
+    useEffect(() => {
+        const boxRef = document.getElementById("chatBox");
+        if (boxRef)
+            boxRef.scrollTop = boxRef.scrollHeight;
+    }, [chats])
+    useEffect(() => {
+        if (socket) {
+            socket.emit("getChats");
+        }
+    }, [socket])
+    useEffect(() => {
+        if (!socket) {
+            const socket = io("http://localhost:3001");
             setSocket(socket);
             return;
         }
-        function sync(currSongArg?:CurrentlyPlaying){
-            if(!currSong)return;
-            let currentSong=currSong;
-            if(currSongArg) currentSong=currSongArg;
-            const seek=(new Date().getTime()-currentSong.start)/1000;
-            const offset=Math.abs(seek-playerRef.current?.seek());
-            if(offset>1) {
+
+        function sync(currSongArg?: CurrentlyPlaying) {
+            if (!currSong) return;
+            let currentSong = currSong;
+            if (currSongArg) currentSong = currSongArg;
+            const seek = (new Date().getTime() - currentSong.start) / 1000;
+            const offset = Math.abs(seek - playerRef.current?.seek());
+            if (offset > 1) {
                 playerRef.current?.seek(seek);
             }
         }
 
-        socket.on("connect",()=>{
-            setInterval(()=>{
-                console.log("emoting")
+        socket.on("connect", () => {
+            setInterval(() => {
                 socket.emit("askSong")
-            },SYNC_RATE)
-            socket.on("receiveSong",(currentlyPlaying:CurrentlyPlaying|null,state:"W"|"P"|"X",nextPlaying:CurrentlyPlaying|null)=>{
-
-                console.log(currentlyPlaying?.song.id,state)
-                if(state==="P"&&currentlyPlaying) {
+            }, SYNC_RATE)
+            socket.on("receiveSong", (currentlyPlaying: CurrentlyPlaying | null, state: "W" | "P" | "X", nextPlaying: CurrentlyPlaying | null) => {
+                if (state === "P" && currentlyPlaying) {
                     setCurrSong(currentlyPlaying);
                     sync(currentlyPlaying)
                     setIsPlaying(true);
                 }
-                if(nextPlaying){
-                    if(!isPlaying&&currSong?.song.id!==nextPlaying.song.id) {
+                if (nextPlaying) {
+                    if (!isPlaying && currSong?.song.id !== nextPlaying.song.id) {
                         setCurrSong(nextPlaying);
                     }
                 }
                 setCurrentState(state);
             })
+            socket.on("newChat", (chats: Chat[]) => {
+                setChats(chats);
+            })
         })
-    },[currSong,isPlaying,socket])
+    }, [currSong, isPlaying, socket])
 
-    function handleSongLoad(){
+    function sendMessage(message: string) {
+        if (!socket || !name) return;
+
+        const chat: Chat = {
+            message,
+            sender: name,
+            id: v4(),
+            time: new Date().toLocaleString()
+        }
+        socket.emit("sendChat", chat)
+
+    }
+
+    function handleSongLoad() {
         setLoaded(true);
     }
-    function handleSongEnd(){
+
+    function handleSongEnd() {
         setIsPlaying(false);
         setLoaded(false);
     }
 
-    function handleSongPlay(){
+    function handleSongPlay() {
         setInteracted(true);
         setIsPlaying(true);
     }
 
-    function handleDivClick(){
+    function handleDivClick() {
         if (!interacted && loaded) {
             playerRef.current?.play();
         }
         setInteracted(true);
+    }
+
+    function handleChatClick() {
+        if (name !== "") {
+            setIsChatModalOpen(true);
+            setIsNameModalOpen(false);
+        } else {
+            setIsNameModalOpen(true);
+            setIsChatModalOpen(false);
+        }
     }
 
     return (
@@ -81,6 +146,25 @@ export default function Player() {
             onClick={handleDivClick}
             className="d-flex min-vh-100 align-items-center justify-content-center"
         >
+            <Modal show={isNameModalOpen} centered onHide={() => setIsNameModalOpen(false)}>
+                <Modal.Header>Enter Name to Chat</Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3">
+                        <Form.Control placeholder="Name" value={name} onChange={e => setName(e.target.value)}/>
+
+                    </Form.Group>
+                    <Form.Group className="mt-3">
+                        <Button onClick={handleChatClick} type="submit">Chat</Button>
+                    </Form.Group>
+
+                </Modal.Body>
+            </Modal>
+            <Modal show={isChatModalOpen} centered onHide={() => setIsChatModalOpen(false)}>
+                <Modal.Header>Messages</Modal.Header>
+                <Modal.Body>
+                    <ChatBox name={name} chats={chats} sendMessage={sendMessage}/>
+                </Modal.Body>
+            </Modal>
             {currSong && (
                 <ReactHowler
                     src={currSong.song.url}
@@ -95,7 +179,7 @@ export default function Player() {
                 <div className="h3">
                     Abhinay's LoFi Radio
                 </div>
-                {isPlaying&&interacted && (
+                {isPlaying && interacted && (
                     <div className="">
                         Currently Playing : {currSong?.song.name}
                     </div>
@@ -105,14 +189,22 @@ export default function Player() {
                         Click anywhere to start the playback!
                     </div>
                 )}
-                {currentState==="W"&&interacted&&
-                    <div className="">
-                        Waiting for the next song
-                    </div>
+                {currentState === "W" && interacted &&
+                <div className="">
+                    Waiting for the next song
+                </div>
                 }
                 {!loaded &&
-                    <div className="spinner-border text-light" role="status"/>
+                <div className="spinner-border text-light m-3" role="status"/>
                 }
+                <hr/>
+                <div className="d-flex justify-content-between align-items-center">
+                    {chats.length > 0 &&
+                    <div>{chats[chats.length - 1].sender} says <i>{chats[chats.length - 1].message.slice(0, 20)}{chats[chats.length - 1].message.length > 20 && "..."}</i>
+                    </div>}
+                    <img alt="chat" className="ms-2 poh" onClick={handleChatClick}
+                         src="https://img.icons8.com/material-outlined/24/000000/chat--v1.png"/>
+                </div>
             </div>
         </div>
     );
