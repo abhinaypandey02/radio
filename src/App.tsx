2f310624c66f73932b11d0aafaf8cbe85edf97b5
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-08-11 12:20:25 +0530

    adding container

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Player from "./pages/player";
 function App() {
     return (
-        <div className="App">
+        <div className="App container">
             <Player />
         </div>
     );
